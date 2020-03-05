df0414d61e973a2aff881979d56304576f6b2fba
Eriold <dacamoty@gmail.com>
2020-03-05 14:31:36 -0500

    Base folders and structure

---
+++ front-mini-translator/src/components/TextLeft.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TextLeft = () => {
+  return <div>Left</div>;
+};
+
+export default TextLeft;
---
+++ front-mini-translator/src/components/TextRight.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TextRight = () => {
+  return <div>Right</div>;
+};
+
+export default TextRight;
